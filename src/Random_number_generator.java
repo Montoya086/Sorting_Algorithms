4d1045618bb4838fa6920e4fe80162c77dfa8e9c
Montoya086 <mon21552@uvg.edu.gt>
2022-02-07 14:36:03 -0600

    Generador de numeros random
    Programa para generar archivo con 3000 numeros random

--- src/Random_number_generator.java
+++ src/Random_number_generator.java
@@ -1,19 +1,29 @@
 /**
  * 
  */
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Iterator;
 
 /**
  * @author Andres
  *
  */
 public class Random_number_generator {
-
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		a
-
+		try {
+			BufferedWriter bw = new BufferedWriter(new FileWriter("random.txt"));
+			for(int i=0;i<3000;i++) {
+				bw.write((int)(Math.random()*(3000-0+1)+0)+"\n");
+			}
+				bw.close();
+		}catch (Exception e) {
+			System.out.println("Ha ocurrido un error de tipo: "+e);
+		}
 	}
 
 }
